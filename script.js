a445b48346df0c91e74b30e55652be5d400669ab
Cristopher <129361337+CrisVA24@users.noreply.github.com>
2026-09-14 21:55:51 -0600

    feat: add initial quiz questions

---
+++ script.js
@@ -0,0 +1,32 @@
+let questions = [
+    {
+        question: "¿Qué lenguaje se utiliza principalmente para agregar interactividad a una página web?",
+        options: ["HTML", "CSS", "JavaScript", "SQL"],
+        correctAnswer: 2
+    },
+    {
+        question: "¿Qué método de JavaScript agrega un elemento al final de un array?",
+        options: ["push()", "pop()", "shift()", "slice()"],
+        correctAnswer: 0
+    },
+    {
+        question: "¿Qué tecnología se utiliza para definir la estructura de una página web?",
+        options: ["CSS", "HTML", "JavaScript", "Git"],
+        correctAnswer: 1
+    },
+    {
+        question: "¿Cuál de las siguientes opciones es un sistema de control de versiones?",
+        options: ["Git", "Node.js", "CSS", "MySQL"],
+        correctAnswer: 0
+    },
+    {
+        question: "¿Qué propiedad del DOM permite obtener un elemento por su identificador?",
+        options: [
+            "document.querySelectorAll()",
+            "document.createElement()",
+            "document.getElementById()",
+            "document.appendChild()"
+        ],
+        correctAnswer: 2
+    }
+];
